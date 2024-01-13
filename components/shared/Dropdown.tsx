494d5db90f5cd1c87c85727ceb0ad16041cd52c7
Erastus Mauliate <emauliate17@gmail.com>
2024-01-13 18:22:26 +0700

    create dropdown and select

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { ICategory } from '@/lib/database/models/category.model';
+import { useState } from 'react';
 
 type DropdownProps = {
-    value?: string,
-    onChangeHandler?: () => void
-}
+  value?: string;
+  onChangeHandler?: () => void;
+};
 
-const Dropdown = ({ value, onChangeHandler}: DropdownProps) => {
+const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+
   return (
-    <div>Dropdown</div>
-  )
-}
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem
+              key={category._id}
+              value={category._id}
+              className="select-item p-regular-14"
+            >
+              {category.name}
+            </SelectItem>
+          ))}
+      </SelectContent>
+    </Select>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
